5bbf3a39a794a9dd5117b985ca0d5eb14ecae5f1
Hiep Mai <hiepknor@gmail.com>
2019-10-16 10:58:59 +0700

    Make Category controller and model for backend

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index() {
-        return view('categories.index');
+    public function index(Category $category) {
+        return view('categories.index', ['categories' => $category->paginate(2)]);
     }
 }
